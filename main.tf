d384be6cc8756ca762953ad050f1eb224bd9ddb3
unknown <Ashok@gmail.com>
2024-06-20 07:44:57 +0530

    3rd

--- main.tf
+++ main.tf
@@ -17,6 +17,14 @@ resource "azurerm_virtual_network" "example" {
   resource_group_name = azurerm_resource_group.example.name
 }
 
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.4.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+
 
 
 
