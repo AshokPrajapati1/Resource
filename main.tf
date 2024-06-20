5c864971373f15ebc1e7d99ec092d6cf3020caf1
unknown <prajapatisaurabh417@gmail.com>
2024-06-20 07:31:38 +0530

    2nd

--- main.tf
+++ main.tf
@@ -10,6 +10,14 @@ resource "azurerm_virtual_network" "example" {
   resource_group_name = azurerm_resource_group.example.name
 }
 
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.3.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+
 
 
  
